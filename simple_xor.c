c23ca48d38de3e41283db21374ce36ed9049e5e7
nep <u76pen@gmail.com>
2026-05-12 23:59:08 +0900

    誤ったバッファ長を指定していたバグを修正

--- simple_xor.c
+++ simple_xor.c
@@ -6,7 +6,7 @@ static int algo_simple_xor( struct XncContext *xnc, unsigned char *restrict buf,
 {
     struct XncSimpleXor *c = (struct XncSimpleXor *)ctx;
 
-    UNUSED(xnc);
+    UNUSED( xnc );
 
     for( size_t i = 0; i < blocks; i++ ){
         for( size_t j = 0; j < sizeof( c->hash ); j++ ){
@@ -32,23 +32,21 @@ int simple_xor( struct XncContext *xnc, FILE *src, uint64_t src_size, FILE *dst 
             xnc_create_salt( xnc, salt, sizeof( salt ) );
             break;
         case XNC_DECODE:
-            src_size -= xnc_read_salt( salt, sizeof( salt ), src );
+            src_size -= xnc_read_salt( salt, sizeof( salt ), src ) * sizeof( salt );
             break;
     }
 
     // パスワードが設定されている場合はキーとパスワードを合わせて
     // もう一度ハッシュ化したものを最終的なキーにする
-    if( xnc->passwd ){
-        size_t pass_len = strlen( xnc->passwd );
-
+    if( xnc->passwd.string ){
         struct {
             char   salt[XNC_SALT_SIZE];
             char passwd[XNC_MAX_PASSWD];
         } __attribute__((packed)) seed;
 
         memcpy( (void *)seed.salt, salt, sizeof( salt ) );
-        memcpy( (void *)seed.passwd, xnc->passwd, pass_len );
-        hash_sha256( xnc, (unsigned char *)&seed, sizeof( seed.salt ) + pass_len, c.hash );
+        memcpy( (void *)seed.passwd, xnc->passwd.string, xnc->passwd.length );
+        hash_sha256( xnc, (unsigned char *)&seed, sizeof( seed.salt ) + xnc->passwd.length, c.hash );
     } else{
         memcpy( c.hash, salt, sizeof( salt ) );
     }
